09e226c05734e6d889638928a733ed4dfb65d2a8
Ed Brown <ed.brown@sas.com>
2017-02-13 17:00:53 -0500

    Parser test added

--- akka.play/src/main/java/com/ekb/cyber/networking/parsers/WebProxyParser.java
+++ akka.play/src/main/java/com/ekb/cyber/networking/parsers/WebProxyParser.java
@@ -4,6 +4,8 @@ import akka.actor.UntypedActor;
 import akka.event.Logging;
 import akka.event.LoggingAdapter;
 
+import java.util.regex.Pattern;
+
 /**
  * Created by ebrown on 2/10/2017.
  */
@@ -11,6 +13,7 @@ public class WebProxyParser extends UntypedActor {
 
     /** logger */
     private final LoggingAdapter log = Logging.getLogger(context().system(), this);
+    private final Pattern pattern = Pattern.compile("<\\d*>(\\w{3}) (\\d{2}:\\d{2}:\\d{2}) (\\w\\d\\.)+ (.*)");
 
     /**
      *
